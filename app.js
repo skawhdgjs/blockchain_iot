f1b5e2a567c6c7c05b599a538fa409f4010650a7
nam <nam@namui-MacBook-Air-2.local>
2017-06-03 14:28:30 +0900

    new

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+// import network parameters for Bitcoin
+var params = require('webcoin-bitcoin').net
+
+// create peer group
+var PeerGroup = require('bitcoin-net').PeerGroup
+var peers = new PeerGroup(params)
 
 
-var privateKey = new bitcore.privateKey();
+
+
+peers.on('peer', (peer) => {
+  console.log('connected to peer', peer.socket.remoteAddress)
+
+  // send/receive messages
+  peer.once('pong', () => console.log('received ping response'))
+  peer.send('ping', {
+    nonce: require('crypto').pseudoRandomBytes(8)
+  })
+  console.log('sent ping')
+})
+
+// create connections to peers
+peers.connect()
+
+// allow incoming connections from bitcoin-net peers
+peers.accept((err) => {
+  if (err) return console.error(err)
+  console.log('accepting incoming connections')
+})
--- block.js
+++
@@ -1,8 +0,0 @@
-exports.hello = function(){
-  console.log('hi');
-}
-
-
-exports.hihi = function(){
-  console.log("HIHI");
-}
